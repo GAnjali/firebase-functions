5d2e3401a75d2dd953b90041fb0dd6afaf9d7517
ganjali <gaddam.anjali@oracle.com>
2019-04-05 08:53:04 +0530

    adding deploy function for notification for new message

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,44 @@ admin.initializeApp(functions.config().firebase);
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
 
+exports.sendPush = functions.database.ref('/registeredUsers/{userId}/chat/{receiverId}').onUpdate((event, context) => {
+    let afterSnapshot = event.after.toJSON();
+    const keys = Object.keys(afterSnapshot);
+    const newMessage = afterSnapshot[keys[keys.length - 1]];
+    const id = newMessage._id;
+    const sender = context.params.userId;
+    const receiver = context.params.receiverId;
+    if (id === 1) {
+        return 0;
+    }
+    let dbUserProfileRef = admin.database().ref("registeredUserProfileInfo").child(receiver);
+            let dbLocalContRef = dbUserProfileRef.child("LocalContacts");
+            let senderName = sender;
+            dbLocalContRef.once("value", (contactsFromDb) =>{
+                if(contactsFromDb.hasChild(sender)){
+                    senderName = contactsFromDb.child(sender).val()
+                    console.log(senderName)
+                }
+            })
+
+    console.log(newMessage, senderName, receiver);
+    admin.database().ref('registeredUserProfileInfo').child(receiver).once("value", (snapshot) => {
+        const token = snapshot.toJSON().pushToken;
+        const payload = {
+            notification: {
+                title: senderName,
+                body: newMessage.text,
+            },
+            data: {
+                sender: senderName,
+                receiver: receiver
+            }
+        }
+        return admin.messaging().sendToDevice(token, payload);
+    });
+    return 0;
+});
+
 exports.sendCallNotification = functions.database.ref('/videoCallInfo/{receiver}/caller')
     .onWrite((event, context )=> {
         let caller = event.after.val();
@@ -12,26 +50,27 @@ exports.sendCallNotification = functions.database.ref('/videoCallInfo/{receiver}
             let receiver = context.params.receiver;
             console.log("receiver:"+receiver);
 
-            let dbUserProfileRef = firebase.database().ref("registeredUserProfileInfo").child(receiver);
+            let dbUserProfileRef = admin.database().ref("registeredUserProfileInfo").child(receiver);
             let dbLocalContRef = dbUserProfileRef.child("LocalContacts");
-            let receiverName = receiverNumber.toString;
+            let callerName = caller.toString;
             dbLocalContRef.once("value", (contactsFromDb) =>{
                 if(contactsFromDb.hasChild(caller)){
-                    receiverName = contactsFromDb.child(caller).val()
+                    callerName = contactsFromDb.child(caller).val()
+                    console.log(callerName)
                 }
             })
 
             admin.database().ref('registeredUserProfileInfo').child(receiver).once("value", (snapshot) => {
                 const token = snapshot.val().pushToken;
-                console.log(token);s
+                console.log(token);
                 const payload = {
                     notification: {
-                        title: receiverName,
+                        title: callerName,
                         body: "Incoming video call. Tap to open",
-                        sound:"Incallmanager_ringtone",
+                        sound:"incallmanager_ringtone",
                     },
                     data:{
-                        sender: caller.toString
+                        sender: callerName
                     }
                 };
                 return admin.messaging().sendToDevice(token, payload);
